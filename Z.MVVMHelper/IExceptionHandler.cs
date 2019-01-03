f57e5fa24b65961e90ae582bcf6bf380cff83c7a
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-01-03 20:31:59 +0100

    Beautified code

--- Z.MVVMHelper/IExceptionHandler.cs
+++ Z.MVVMHelper/IExceptionHandler.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿#region USINGS
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+#endregion
 
 namespace Z.MVVMHelper
 {
     public interface IExceptionHandler
     {
         void HandleException<TExc>(TExc exception) where TExc : Exception;
-
     }
 }
