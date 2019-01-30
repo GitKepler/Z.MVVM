9e01caa65db666c81802ead86b6568e8a8027a4e
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-01-30 20:35:03 +0100

    Commented VmCommand & AsyncVmCommand

--- Z.MVVMHelper/AsyncVmCommand.cs
+++ Z.MVVMHelper/AsyncVmCommand.cs
@@ -2,35 +2,88 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using JetBrains.Annotations;
+using ICommand = Z.MVVMHelper.Interfaces.ICommand;
 
 #endregion
 
 namespace Z.MVVMHelper
 {
-    public class AsyncVmCommand<TParam> : Interfaces.ICommand
+    /// <summary>
+    ///     Asynchronous MVVM Command
+    /// </summary>
+    /// <typeparam name="TParam"></typeparam>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBeProtected.Global")]
+    [SuppressMessage("ReSharper", "EventNeverSubscribedTo.Global")]
+    public class AsyncVmCommand<TParam> : ICommand
     {
-        [NotNull] protected static readonly Func<TParam, bool> AlwaysEnabled = _ => true;
-        [NotNull] private readonly Func<TParam, bool> _canExecute;
+        /// <summary>
+        ///     Always enable the command
+        /// </summary>
+        [NotNull] public static readonly Predicate<TParam> AlwaysEnabled = _ => true;
+
+        [NotNull] private readonly Predicate<TParam> _canExecute;
         [NotNull] private readonly Func<TParam, Task> _execute;
         private bool? _isEnabled;
 
-        public AsyncVmCommand([NotNull] Func<TParam, bool> canExecute, [NotNull] Func<TParam, Task> execute) {
+        /// <summary>
+        ///     Create a <see cref="AsyncVmCommand{TParam}" /> with custom values for every field taking in argument the binding
+        ///     parameters
+        /// </summary>
+        /// <param name="canExecute"><see cref="Predicate{T}" /> determining whether the command can be executed</param>
+        /// <param name="execute">
+        ///     <see cref="Func{TResult}" /> determining the what the command is doing. The returned value is the
+        ///     <see cref="Task" /> to await
+        /// </param>
+        public AsyncVmCommand([NotNull] Predicate<TParam> canExecute, [NotNull] Func<TParam, Task> execute) {
             _canExecute = canExecute;
             _execute = execute;
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Create a <see cref="AsyncVmCommand{TParam}" /> with custom values for every field
+        /// </summary>
+        /// <param name="canExecute"><see cref="Func{TResult}" /> determining whether the command can be executed</param>
+        /// <param name="execute">
+        ///     <see cref="Func{TResult}" /> determining the what the command is doing. The returned value is the
+        ///     <see cref="Task" /> to await
+        /// </param>
         public AsyncVmCommand([NotNull] Func<bool> canExecute, [NotNull] Func<Task> execute) : this(
             _ => canExecute(),
             _ => execute()) { }
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Create a <see cref="AsyncVmCommand{TParam}" /> with custom values for the action to execute and taking in argument
+        ///     the
+        ///     binding parameters
+        /// </summary>
+        /// <param name="execute">
+        ///     <see cref="Func{TResult}" /> determining the what the command is doing. The returned value is the
+        ///     <see cref="Task" /> to await
+        /// </param>
         public AsyncVmCommand([NotNull] Func<Task> execute) : this(AlwaysEnabled, _ => execute()) { }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Create a <see cref="VmCommand{TParam}" /> with custom values for the action to execute
+        /// </summary>
+        /// <param name="execute">
+        ///     <see cref="Func{TResult}" /> determining the what the command is doing. The returned value is the
+        ///     <see cref="Task" /> to await
+        /// </param>
         public AsyncVmCommand([NotNull] Func<TParam, Task> execute) : this(AlwaysEnabled, execute) { }
 
+        /// <summary>
+        ///     Check whether the command can be run and force an override (one-time)
+        /// </summary>
         public bool IsEnabled {
             get => CanExecute(null);
             set {
@@ -39,11 +92,24 @@ namespace Z.MVVMHelper
             }
         }
 
+        /// <summary>
+        ///     Handler whenever a method throws an exception
+        /// </summary>
         [CanBeNull]
         public IExceptionHandler ExceptionHandler { get; set; }
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Event triggered whenever <see cref="M:Z.MVVMHelper.VmCommand`1.CanExecute(System.Object)" /> might return another
+        ///     value
+        /// </summary>
         public event EventHandler CanExecuteChanged;
 
+        /// <summary>
+        ///     Returns whether the command might or not be able to run depending on the binding parameter
+        /// </summary>
+        /// <param name="parameter">Binding parameter</param>
+        /// <returns>Whether the command can run</returns>
         public bool CanExecute([CanBeNull] object parameter) {
             if (_isEnabled is null) {
                 if (parameter is TParam t) {
@@ -57,6 +123,11 @@ namespace Z.MVVMHelper
                 $"{nameof(parameter)} ({parameter?.GetType().Name ?? "null"}) is incompatible with {nameof(TParam)}");
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Execute the command with the binding parameter as argument
+        /// </summary>
+        /// <param name="parameter"></param>
         public void Execute([CanBeNull] object parameter) {
             if (!(parameter is TParam t)) {
                 throw new ArgumentException(
@@ -74,24 +145,54 @@ namespace Z.MVVMHelper
             }
         }
 
+        /// <summary>
+        ///     Indicate that the command's <see cref="CanExecute" /> return value might have changed
+        /// </summary>
         public void Refresh() {
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        ///     Current state of execution of the executed <see cref="Task" />
+        /// </summary>
         public event EventHandler<ExecutingEventArgs> Executing;
     }
 
+    /// <inheritdoc />
+    /// <summary>
+    ///     Asynchronous MVVM Command
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public class AsyncVmCommand : AsyncVmCommand<object>
     {
-        public AsyncVmCommand([NotNull] Func<object, bool> canExecute, [NotNull] Func<object, Task> execute) : base(
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="canExecute"></param>
+        /// <param name="execute"></param>
+        public AsyncVmCommand([NotNull] Predicate<object> canExecute, [NotNull] Func<object, Task> execute) : base(
             canExecute,
             execute) { }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="canExecute"></param>
+        /// <param name="execute"></param>
         public AsyncVmCommand([NotNull] Func<bool> canExecute, [NotNull] Func<Task> execute) : base(
             _ => canExecute(),
             _ => execute()) { }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="execute"></param>
         public AsyncVmCommand([NotNull] Func<Task> execute) : base(AlwaysEnabled, _ => execute()) { }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="execute"></param>
         public AsyncVmCommand([NotNull] Func<object, Task> execute) : base(AlwaysEnabled, execute) { }
     }
 }
--- Z.MVVMHelper/VmCommand.cs
+++ Z.MVVMHelper/VmCommand.cs
@@ -9,29 +9,63 @@ using ICommand = Z.MVVMHelper.Interfaces.ICommand;
 
 namespace Z.MVVMHelper
 {
+    /// <summary>
+    ///     Synchronous MVVM Command
+    /// </summary>
+    /// <typeparam name="TParam">Type of the expected</typeparam>
     [SuppressMessage("ReSharper", "MemberCanBeProtected.Global")]
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     [SuppressMessage("ReSharper", "UnusedParameter.Local")]
     public class VmCommand<TParam> : ICommand
     {
-        [NotNull] protected static readonly Predicate<TParam> AlwaysEnabled = _ => true;
+        /// <summary>
+        ///     Always enable the command
+        /// </summary>
+        [NotNull] public static readonly Predicate<TParam> AlwaysEnabled = _ => true;
+
         [NotNull] private readonly Predicate<TParam> _canExecute;
         [NotNull] private readonly Action<TParam> _execute;
         private bool? _isEnabled;
 
+        /// <summary>
+        ///     Create a <see cref="VmCommand{TParam}" /> with custom values for every field taking in argument the binding
+        ///     parameters
+        /// </summary>
+        /// <param name="canExecute"><see cref="Predicate{T}" /> determining whether the command can be executed</param>
+        /// <param name="execute"><see cref="Action{T}" /> determining the what the command is doing</param>
         public VmCommand([NotNull] Predicate<TParam> canExecute, [NotNull] Action<TParam> execute) {
             _execute = execute;
             _canExecute = canExecute;
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Create a <see cref="VmCommand{TParam}" /> with custom values for the action to execute and taking in argument the
+        ///     binding parameters
+        /// </summary>
+        /// <param name="execute"><see cref="Action{T}" /> determining the what the command is doing</param>
         public VmCommand([NotNull] Action<TParam> execute) : this(AlwaysEnabled, execute) { }
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Create a <see cref="VmCommand{TParam}" /> with custom values for every field
+        /// </summary>
+        /// <param name="canExecute"><see cref="Func{TResult}" /> determining whether the command can be executed</param>
+        /// <param name="execute"><see cref="Action" /> determining the what the command is doing</param>
         public VmCommand([NotNull] Func<bool> canExecute, [NotNull] Action execute) : this(
             _ => canExecute(),
             _ => canExecute()) { }
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Create a <see cref="VmCommand{TParam}" /> with custom values for the action to execute
+        /// </summary>
+        /// <param name="execute"><see cref="Action" /> determining the what the command is doing</param>
         public VmCommand([NotNull] Action execute) : this(AlwaysEnabled, _ => execute()) { }
 
+        /// <summary>
+        ///     Check whether the command can be run and force an override (one-time)
+        /// </summary>
         public bool IsEnabled {
             get => CanExecute(null);
             set {
@@ -40,57 +74,110 @@ namespace Z.MVVMHelper
             }
         }
 
+        /// <summary>
+        ///     Handler whenever a method throws an exception
+        /// </summary>
         public IExceptionHandler ExceptionHandler { get; set; }
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Event triggered whenever <see cref="M:Z.MVVMHelper.VmCommand`1.CanExecute(System.Object)" /> might return another
+        ///     value
+        /// </summary>
         public event EventHandler CanExecuteChanged;
 
+        /// <summary>
+        ///     Returns whether the command might or not be able to run depending on the binding parameter
+        /// </summary>
+        /// <param name="parameter">Binding parameter</param>
+        /// <returns>Whether the command can run</returns>
         public bool CanExecute([CanBeNull] object parameter) {
             if (_isEnabled is null) {
-                if (parameter is TParam t) {
-                    return _canExecute(t);
+                switch (parameter) {
+                    case TParam t:
+                        return _canExecute(t);
+                    case null:
+                        return _canExecute(default(TParam));
                 }
             } else {
-                return _isEnabled.Value;
+                bool val = _isEnabled.Value;
+                _isEnabled = null;
+                return val;
+            }
+
+            throw new ArgumentException(
+                $"{nameof(parameter)} ({parameter?.GetType().Name}) is incompatible with {nameof(TParam)}");
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Execute the command with the binding parameter as argument
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute([CanBeNull] object parameter) {
+            if (!(parameter is TParam) && !(parameter is null)) {
+                throw new ArgumentException(
+                    $"{nameof(parameter)} ({parameter?.GetType().Name}) is incompatible with {nameof(TParam)}");
+            }
+
+            try {
+                if (parameter is null) {
+                    _execute(default(TParam));
+                } else {
+                    _execute((TParam) parameter);
+                }
+            } catch (Exception ex) {
+                ExceptionHandler?.HandleException(ex);
             }
 
             throw new ArgumentException(
                 $"{nameof(parameter)} ({parameter?.GetType().Name ?? "null"}) is incompatible with {nameof(TParam)}");
         }
 
-        public void Execute([CanBeNull] object parameter) {
-            if (!(parameter is TParam t)) {
-                throw new ArgumentException(
-                    $"{nameof(parameter)} ({parameter?.GetType().Name ?? "null"}) is incompatible with {nameof(TParam)}");
-            }
-
-            try {
-                _execute(t);
-            } catch (Exception ex) {
-                ExceptionHandler?.HandleException(ex);
-            }
-
-            throw new ArgumentException(
-                $"{nameof(parameter)} ({parameter.GetType().Name}) is incompatible with {nameof(TParam)}");
-        }
-
+        /// <summary>
+        ///     Indicate that the command's <see cref="CanExecute" /> return value might have changed
+        /// </summary>
         public void Refresh() {
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
     }
 
+    /// <inheritdoc />
+    /// <summary>
+    ///     Synchronous MVVM Command
+    /// </summary>
     [SuppressMessage("ReSharper", "UnusedParameter.Local")]
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public class VmCommand : VmCommand<object>
     {
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="canExecute"></param>
+        /// <param name="execute"></param>
         public VmCommand([NotNull] Predicate<object> canExecute, [NotNull] Action<object> execute) : base(
             canExecute,
             execute) { }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="canExecute"></param>
+        /// <param name="execute"></param>
         public VmCommand([NotNull] Func<bool> canExecute, [NotNull] Action execute) : base(
             _ => canExecute(),
             _ => canExecute()) { }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="execute"></param>
         public VmCommand([NotNull] Action execute) : base(AlwaysEnabled, _ => execute()) { }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="execute"></param>
         public VmCommand([NotNull] Action<object> execute) : base(execute) { }
     }
 }
