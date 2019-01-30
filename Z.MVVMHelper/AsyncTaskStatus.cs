961eaa991da7dfe96610fe03ffe9f7b69c0c831a
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-01-30 20:36:08 +0100

    Commented AsyncTaskStatus

--- Z.MVVMHelper/AsyncTaskStatus.cs
+++ Z.MVVMHelper/AsyncTaskStatus.cs
@@ -1,9 +1,29 @@
-﻿namespace Z.MVVMHelper
+﻿#region USINGS
+
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Z.MVVMHelper
 {
+    /// <summary>
+    ///     Status of a <see cref="Task" />
+    /// </summary>
     public enum AsyncTaskStatus
     {
+        /// <summary>
+        ///     The task is starting
+        /// </summary>
         Starting,
+
+        /// <summary>
+        ///     The task has not yet been awaited
+        /// </summary>
         Started,
+
+        /// <summary>
+        ///     The task has been awaited
+        /// </summary>
         Ended
     }
 }
