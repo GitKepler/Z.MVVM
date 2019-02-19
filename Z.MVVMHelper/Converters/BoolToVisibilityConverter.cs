5af4a916d112afd3afa35098601326719064b2f4
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-02-19 21:47:43 +0100

    Created converters (bool to visibility & inverter)

---
+++ Z.MVVMHelper/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using JetBrains.Annotations;
+
+namespace Z.MVVMHelper.Converters
+{
+    /// <summary>
+    /// Convert a <see cref="bool"/> to a <see cref="Visibility"/> and back
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        /// <inheritdoc />
+        [NotNull]
+        public object Convert(object value, [CanBeNull] Type targetType, object parameter, [CanBeNull] CultureInfo culture) {
+            if (value is bool b) {
+                return b ? Visibility.Visible : Visibility.Hidden;
+            }
+
+            throw Internals.ExceptionGenerator.InvalidArgumentType<bool>(value?.GetType(), nameof(value));
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, [CanBeNull] Type targetType, object parameter, [CanBeNull] CultureInfo culture) {
+            if (value is Visibility v) {
+                return v > 0;
+            }
+
+            throw Internals.ExceptionGenerator.InvalidArgumentType<Visibility>(value?.GetType(), nameof(value));
+        }
+
+        #endregion
+    }
+}
---
+++ Z.MVVMHelper/Converters/InverterConverter.cs
@@ -0,0 +1,55 @@
+﻿#region USINGS
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using JetBrains.Annotations;
+
+#endregion
+
+namespace Z.MVVMHelper.Converters
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Perform an inversion of a <see cref="bool"/> value
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class InverterConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Convert the value to its opposite
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, [CanBeNull] Type targetType, object parameter,
+            [CanBeNull] CultureInfo culture) {
+            if (value is bool b) {
+                return !b;
+            }
+
+            throw Internals.ExceptionGenerator.InvalidArgumentType<bool>(value?.GetType(), nameof(value));
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Convert back the value to the original
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, [CanBeNull] Type targetType, object parameter, [CanBeNull] CultureInfo culture) {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
