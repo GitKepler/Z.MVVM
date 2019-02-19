6fa51ba784a3996ac3514ddcb46079af1aabccae
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-02-19 23:08:52 +0100

    Added property getter & setter when property has anchorattribute

---
+++ Z.MVVMHelper/AnchorAttribute.cs
@@ -0,0 +1,18 @@
+﻿#region USINGS
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Z.MVVMHelper
+{
+    /// <inheritdoc />
+    [AttributeUsage(AttributeTargets.Property)]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class AnchorAttribute : Attribute { }
+}
--- Z.MVVMHelper/VmBase.cs
+++ Z.MVVMHelper/VmBase.cs
@@ -1,10 +1,16 @@
 ﻿#region USINGS
 
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
+using Z.MVVMHelper.Internals;
 
 #endregion
 
@@ -20,6 +26,12 @@ namespace Z.MVVMHelper
     // ReSharper disable once InconsistentNaming
     public abstract class VMBase : INotifyPropertyChanged, INotifyPropertyChanging
     {
+        [NotNull] private static readonly ConcurrentDictionary<Type, object> _defaultValues =
+            new ConcurrentDictionary<Type, object>();
+
+        [NotNull] private readonly ConcurrentDictionary<string, object> _backingfields =
+            new ConcurrentDictionary<string, object>();
+
         /// <inheritdoc />
         /// <summary>
         ///     When a property is changed
@@ -32,13 +44,36 @@ namespace Z.MVVMHelper
         /// </summary>
         public event PropertyChangingEventHandler PropertyChanging;
 
+        public void Initialize() {
+            IEnumerable<PropertyInfo> props = GetType()
+                .GetProperties()
+                .Where(prop => prop.CustomAttributes?.Any(a => a?.AttributeType == typeof(AnchorAttribute)) ?? false);
+            foreach (PropertyInfo propertyInfo in props) {
+                if (!_defaultValues.ContainsKey(propertyInfo.PropertyType) && propertyInfo.PropertyType.IsValueType) {
+                    Func<object> getDefault = Expression.Lambda<Func<object>>(
+                            Expression.Convert(Expression.Default(propertyInfo.PropertyType), typeof(object)))
+                        .Compile();
+                    object deft = getDefault();
+                    _defaultValues.TryAdd(propertyInfo.PropertyType, deft);
+                } else if (propertyInfo.PropertyType.IsValueType) {
+                    _defaultValues.TryAdd(propertyInfo.PropertyType, null);
+                }
+
+
+                _defaultValues.TryGetValue(propertyInfo.PropertyType, out object propVal);
+                _backingfields.TryAdd(propertyInfo.Name, propVal);
+                MethodInfo setter = propertyInfo.GetSetMethod();
+                MethodInfo getter = propertyInfo.GetGetMethod();
+            }
+        }
+
         /// <summary>
         ///     Called before changing a property value
         /// </summary>
         /// <param name="propName">The name of the property</param>
         protected void OnPropertyChanging([NotNull] [CallerMemberName] string propName = "") {
             if (propName == null) {
-                throw Internals.ExceptionGenerator.ArgumentNull(nameof(propName));
+                throw ExceptionGenerator.ArgumentNull(nameof(propName));
             }
 
             DelegatePropertyChanging(propName);
@@ -50,7 +85,7 @@ namespace Z.MVVMHelper
         /// <param name="propName">The name of the property</param>
         protected void OnPropertyChanged([NotNull] [CallerMemberName] string propName = "") {
             if (propName == null) {
-                throw Internals.ExceptionGenerator.ArgumentNull(nameof(propName));
+                throw ExceptionGenerator.ArgumentNull(nameof(propName));
             }
 
             DelegatePropertyChanged(propName);
@@ -81,7 +116,7 @@ namespace Z.MVVMHelper
         /// <param name="propName">The property being changed</param>
         protected void DelegatePropertyChanging([CanBeNull] string propName) {
             if (string.IsNullOrWhiteSpace(propName)) {
-                throw Internals.ExceptionGenerator.ArgumentNullOrEmpty( nameof(propName));
+                throw ExceptionGenerator.ArgumentNullOrEmpty(nameof(propName));
             }
 
             PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propName));
@@ -93,7 +128,7 @@ namespace Z.MVVMHelper
         /// <param name="propName">The property after the changes has been made</param>
         protected void DelegatePropertyChanged([CanBeNull] string propName) {
             if (string.IsNullOrWhiteSpace(propName)) {
-                throw Internals.ExceptionGenerator.ArgumentNullOrEmpty( nameof(propName));
+                throw ExceptionGenerator.ArgumentNullOrEmpty(nameof(propName));
             }
 
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
