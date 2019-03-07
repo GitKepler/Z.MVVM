1331e60492625ea9679363c55df165cadc80422b
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-03-07 23:13:17 +0100

    Added CommandRepository

---
+++ Z.MVVMHelper/CommandRepository.cs
@@ -0,0 +1,33 @@
+﻿#region USINGS
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Z.MVVMHelper.Interfaces;
+
+#endregion
+
+namespace Z.MVVMHelper
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     Group commands in VMs
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class CommandRepository : List<ICommand>
+    {
+        /// <summary>
+        /// Assign a specific <see cref="IExceptionHandler"/> to every command in the current <see cref="CommandRepository"/>
+        /// </summary>
+        /// <param name="handler">Exception handler</param>
+        public void AssignExceptionHandler([CanBeNull] IExceptionHandler handler) {
+            foreach (ICommand command in this.Where(c => !(c is null))) {
+                command.ExceptionHandler = handler;
+            }
+        }
+    }
+}
