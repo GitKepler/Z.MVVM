346ecbb3937c9177012fbea4cd86ad3b29b4db35
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-03-07 22:10:40 +0100

    Added SynchronousCommandBase as the abstract class for synchronous commands

---
+++ Z.MVVMHelper/Commands/SynchronousCommandBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Z.MVVMHelper.Commands
+{
+    public abstract class SynchronousCommandBase : CommandBase
+    {
+        /// <inheritdoc />
+        public SynchronousCommandBase([NotNull] Predicate<object> canExecute) : base(canExecute) { }
+
+        #region Overrides of CommandBase
+
+        /// <inheritdoc />
+        protected override void Run(object parameter) {
+            MethodStart();
+            try {
+                RunSynchronously(parameter);
+            } finally {
+                MethodEnd();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        protected abstract void RunSynchronously([CanBeNull] object parameter);
+
+        #endregion
+    }
+}
