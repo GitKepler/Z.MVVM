195f997591afd2c5174aa7382dce6ae2aa0c931e
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-03-09 14:10:08 +0100

    Added interface for common validator attributes

---
+++ Z.MVVMHelper/Interfaces/IValidatorAttribute.cs
@@ -0,0 +1,31 @@
+﻿#region USINGS
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+#endregion
+
+namespace Z.MVVMHelper.Interfaces
+{
+    /// <summary>
+    ///     Interface for validator attributes
+    /// </summary>
+    public interface IValidatorAttribute
+    {
+        /// <summary>
+        ///     Validator
+        /// </summary>
+        [NotNull]
+        Func<object, string> ErrorGenerator { get; }
+
+        /// <summary>
+        /// Name of the property the attribute is applied to
+        /// </summary>
+        [NotNull]
+        string PropertyName { get; }
+    }
+}
