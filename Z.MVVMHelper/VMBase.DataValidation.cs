505208674d397fdbe1aab292b5ebf7da436c7100
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-03-09 14:45:00 +0100

    Implemented IDataErrorInfo

--- Z.MVVMHelper/VMBase.DataValidation.cs
+++ Z.MVVMHelper/VMBase.DataValidation.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -17,17 +18,32 @@ namespace Z.MVVMHelper
 {
     public abstract partial class VmBase : IDataErrorInfo
     {
+        [CanBeNull] private string _error;
+
         [NotNull]
         private ConcurrentDictionary<string, string> ValidationErrors { get; } = new ConcurrentDictionary<string, string>();
 
         [NotNull]
         private ConcurrentDictionary<string, List<IValidatorAttribute>> ValidationAttributes { get; } = new ConcurrentDictionary<string, List<IValidatorAttribute>>();
 
+        /// <summary>
+        ///     If the VM is valid
+        /// </summary>
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        public bool IsValid => string.IsNullOrWhiteSpace(Error);
+
         /// <inheritdoc />
         /// <summary>
         ///     Validation errors
         /// </summary>
-        public string Error => throw new NotImplementedException();
+        [CanBeNull]
+        public string Error {
+            get => _error;
+            private set {
+                EditProperty(ref _error, value);
+                DelegatePropertyChanged(nameof(IsValid));
+            }
+        }
 
         /// <inheritdoc />
         /// <summary>
@@ -35,7 +51,32 @@ namespace Z.MVVMHelper
         /// </summary>
         /// <param name="columnName">Name of the member</param>
         /// <returns></returns>
-        public string this[string columnName] => throw new NotImplementedException();
+        [NotNull]
+        public string this[[CanBeNull] string columnName] {
+            get {
+                if (columnName is null) {
+                    foreach (KeyValuePair<string, List<IValidatorAttribute>> validationAttribute in ValidationAttributes) {
+                        string _ = this[validationAttribute.Key];
+                    }
+                }
+
+                object value = GetType().GetProperty(columnName)?.GetValue(this);
+                bool isOk = ValidationAttributes.TryGetValue(columnName, out List<IValidatorAttribute> validators);
+                if (!isOk) {
+                    return string.Empty;
+                }
+
+                string errors = validators
+                    .Select(a => a?.ErrorGenerator)
+                    .Where(g => !(g is null))
+                    .Aggregate(new StringBuilder(), (builder, generator) => builder.AppendLine(generator(value)))
+                    .ToString()
+                    .Trim();
+                ValidationErrors.AddOrUpdate(columnName, errors, (s, s1) => errors);
+                Error = string.Join(Environment.NewLine, ValidationErrors.ToArray().Select(kvp => $"[{kvp.Key}]{Environment.NewLine}{kvp.Value}"));
+                return errors;
+            }
+        }
 
         private void InitializeDataValidator() {
             IEnumerable<IValidatorAttribute> props = GetType().GetProperties().SelectMany(p => p.GetCustomAttributes(typeof(IValidatorAttribute), true).OfType<IValidatorAttribute>());
