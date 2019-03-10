252793695f1d4ef7a18ab829207bc85a20b49305
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-03-10 14:58:57 +0100

    Added basic tests

---
+++ Z.MVVMHelperTests/VmBaseTests.cs
@@ -0,0 +1,50 @@
+﻿#region USINGS
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Z.MVVMHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Z.MVVMHelperTests;
+
+#endregion
+
+namespace Z.MVVMHelper.Tests
+{
+    [TestClass]
+    public class VmBaseTests
+    {
+        [NotNull] private FakeVM vm;
+
+        [TestInitialize]
+        public void Init() {
+            vm = new FakeVM();
+        }
+
+        [TestCleanup]
+        public void Cleanup() { }
+
+        [TestMethod]
+        public void TestRegexValidationNotPass() {
+            vm.CheckedRegex = "123456";
+            string error = vm.Errors[nameof(vm.CheckedRegex)];
+            Assert.AreEqual($"\"{vm.CheckedRegex}\" is not a valid value for {nameof(vm.CheckedRegex)}", error);
+        }
+
+        [TestMethod]
+        public void TestRegexValidationPass() {
+            vm.CheckedRegex = "ABC123";
+            string error = vm.Errors[nameof(vm.CheckedRegex)];
+            Assert.IsTrue(string.IsNullOrWhiteSpace(error));
+        }
+
+        public void TestCustomValidationWithTextNotPass() {
+            vm.CheckedExpression1 = "abcdef";
+            string error = vm.Errors[nameof(vm.CheckedExpression1)];
+            Assert.AreEqual("String length must be < 10", error);
+        }
+    }
+}
