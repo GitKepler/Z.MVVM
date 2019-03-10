3516628ff777f709996ef1975a1f21c54334e297
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-03-10 13:31:12 +0100

    Added NotifyDataError default implementation

---
+++ Z.MVVMHelper/VmBase.NotifyDataError.cs
@@ -0,0 +1,25 @@
+﻿#region USINGS
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Z.MVVMHelper
+{
+    public abstract partial class VmBase : INotifyDataErrorInfo
+    {
+        public bool HasErrors => !IsValid;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName) {
+            throw new NotImplementedException();
+        }
+    }
+}
