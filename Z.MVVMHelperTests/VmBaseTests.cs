82d433422c85c64b4e6b38f9d2e42ad9d7359111
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-03-15 18:49:07 +0100

    Removed FakeVM external file

--- Z.MVVMHelperTests/VmBaseTests.cs
+++ Z.MVVMHelperTests/VmBaseTests.cs
@@ -8,7 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
-using Z.MVVMHelperTests;
+using Z.MVVMHelper.Commands;
+using Z.MVVMHelper.Validation;
 
 #endregion
 
@@ -20,53 +21,111 @@ namespace Z.MVVMHelper.Tests
         [NotNull] private FakeVM vm;
 
         [TestInitialize]
-        public void Init() {
-            vm = new FakeVM();
-        }
+        public void Init() { }
 
         [TestCleanup]
         public void Cleanup() { }
 
         [TestMethod]
         public void TestRegexValidationNotPass() {
-            vm.CheckedRegex = "123456";
-            string error = vm.Errors[nameof(vm.CheckedRegex)];
+            vm = new FakeVM {
+                CheckedRegex = "123456"
+            };
+            Assert.IsTrue(vm.Errors.ContainsKey(nameof(vm.CheckedRegex)));
+            string error = vm.Errors[nameof(vm.CheckedRegex)]?.FirstOrDefault();
+            Assert.IsNotNull(error);
             Assert.AreEqual($"\"{vm.CheckedRegex}\" is not a valid value for {nameof(vm.CheckedRegex)}", error);
+            Assert.IsTrue(vm.HasErrors);
         }
 
         [TestMethod]
         public void TestRegexValidationPass() {
-            vm.CheckedRegex = "ABC123";
-            string error = vm.Errors[nameof(vm.CheckedRegex)];
-            Assert.IsTrue(string.IsNullOrWhiteSpace(error));
+            vm = new FakeVM {
+                CheckedRegex = "ABC123"
+            };
+            if (vm.Errors.ContainsKey(nameof(vm.CheckedRegex))) {
+                IReadOnlyList<string> value = vm.Errors[nameof(vm.CheckedRegex)];
+                Assert.IsNotNull(value);
+                Assert.AreEqual(0, value.Count);
+            }
+
+            Assert.IsFalse(vm.HasErrors);
         }
 
         [TestMethod]
         public void TestCustomValidationWithTextNotPass() {
-            vm.CheckedExpression2 = "abcdef";
-            string error = vm.Errors[nameof(vm.CheckedExpression2)];
+            vm = new FakeVM {
+                CheckedExpression2 = "abcdef"
+            };
+            Assert.IsTrue(vm.Errors.ContainsKey(nameof(vm.CheckedExpression2)));
+            string error = vm.Errors[nameof(vm.CheckedExpression2)]?.FirstOrDefault();
+            Assert.IsNotNull(error);
             Assert.AreEqual("String length must be < 10", error);
+            Assert.IsTrue(vm.HasErrors);
         }
 
         [TestMethod]
         public void TestCustomValidationWithTextPass() {
-            vm.CheckedExpression2 = "abcdefghijklmn";
-            string error = vm.Errors[nameof(vm.CheckedExpression2)];
-            Assert.IsTrue(string.IsNullOrWhiteSpace(error));
+            vm = new FakeVM {
+                CheckedExpression2 = "abcdefghijklmn"
+            };
+            if (vm.Errors.ContainsKey(nameof(vm.CheckedExpression2))) {
+                IReadOnlyList<string> value = vm.Errors[nameof(vm.CheckedExpression2)];
+                Assert.IsNotNull(value);
+                Assert.AreEqual(0, value.Count);
+            }
+
+            Assert.IsFalse(vm.HasErrors);
         }
 
         [TestMethod]
         public void TestCustomValidationWithoutTextNotPass() {
-            vm.CheckedExpression1 = "abcdefghijklmn";
-            string error = vm.Errors[nameof(vm.CheckedExpression1)];
-            Assert.AreEqual(error, "s => s.Length > 10");
+            vm = new FakeVM {
+                CheckedExpression1 = "abcdef"
+            };
+            Assert.IsTrue(vm.Errors.ContainsKey(nameof(vm.CheckedExpression1)));
+            string error = vm.Errors[nameof(vm.CheckedExpression1)]?.FirstOrDefault();
+            Assert.IsNotNull(error);
+            Assert.AreEqual("s => s.Length > 10 did not pass.", error);
+            Assert.IsTrue(vm.HasErrors);
         }
 
         [TestMethod]
         public void TestCustomValidationWithoutTextPass() {
-            vm.CheckedExpression1 = "abcdef";
-            string error = vm.Errors[nameof(vm.CheckedExpression1)];
-            Assert.IsTrue(string.IsNullOrWhiteSpace(error));
+            vm = new FakeVM {
+                CheckedExpression1 = "abcdefghijklmnop"
+            };
+            if (vm.Errors.ContainsKey(nameof(vm.CheckedExpression1))) {
+                IReadOnlyList<string> value = vm.Errors[nameof(vm.CheckedExpression1)];
+                Assert.IsNotNull(value);
+                Assert.AreEqual(0, value.Count);
+            }
+
+            Assert.IsFalse(vm.HasErrors);
+        }
+
+        public class FakeVM : VmBase
+        {
+            private string _checkedExpression1;
+            private string _checkedExpression2;
+            private string _checkedRegex;
+            private string _notChecked;
+
+            public FakeVM() {
+                RegisterValidator(new CustomValidator<string>(nameof(CheckedExpression1), s => s.Length > 10));
+                RegisterValidator(new CustomValidator<string>(nameof(CheckedExpression2), s => s.Length < 10, _ => "String length must be < 10"));
+            }
+
+            public string NotChecked { get => _notChecked; set => EditProperty(ref _notChecked, value); }
+
+            [RegexValidation(nameof(CheckedRegex), "^ABC.*$", AllowNull = false)]
+            public string CheckedRegex { get => _checkedRegex; set => EditProperty(ref _checkedRegex, value); }
+
+            public string CheckedExpression1 { get => _checkedExpression1; set => EditProperty(ref _checkedExpression1, value); }
+
+            public string CheckedExpression2 { get => _checkedExpression2; set => EditProperty(ref _checkedExpression2, value); }
+
+            public Command Command { get; }
         }
     }
 }
