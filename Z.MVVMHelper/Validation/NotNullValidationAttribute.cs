ade89f068f7b6b97db9d8058ac5d0629030a09de
GitKepler <23261056+GitKepler@users.noreply.github.com>
2019-03-25 19:24:25 +0100

    Added attribute

---
+++ Z.MVVMHelper/Validation/NotNullValidationAttribute.cs
@@ -0,0 +1,36 @@
+﻿#region USINGS
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Z.MVVMHelper.Internals;
+
+#endregion
+
+namespace Z.MVVMHelper.Validation
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     Null checking validation rule
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public sealed class NotNullValidationAttribute : BaseValidationAttribute
+    {
+        /// <inheritdoc />
+        public NotNullValidationAttribute([NotNull] string propertyName) : base(propertyName) {
+            ValueValidator.ArgumentNull(propertyName, nameof(propertyName));
+            ErrorGenerator = o => o is null ? $"The value of {propertyName} is null" : string.Empty;
+        }
+
+        #region Overrides of BaseValidationAttribute
+
+        /// <inheritdoc />
+        public override Func<object, string> ErrorGenerator { get; }
+
+        #endregion
+    }
+}
